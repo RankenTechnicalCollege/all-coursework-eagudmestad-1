4abffcba2081d34c018623b5324a869df4a99fa6
Evan Gudmestad <eagudmestad@ranken.edu>
2020-01-13 12:37:02 -0600

    Completed CH2 Labs

---
+++ Lab/CH2/Ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output
+            Console.WriteLine("What is your name?");
+
+            //Input from the user
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your salary?");
+            float salary = Convert.ToSingle(Console.ReadLine());
+
+            salary = salary * 2;
+
+            //Output the name to the Console
+            Console.WriteLine("Hello {0} your doubled salary is {1:C}",name,salary);
+        }
+    }
+}
---
+++ Lab/CH2/Ex2/frmInvoice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex2
+{
+    public partial class frmInvoice : Form
+    {
+        public frmInvoice()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
