3c33abf3c8eb998dbe4ee3e35dad65b33dfc68b0
Evan Gudmestad <eagudmestad@ranken.edu>
2020-01-21 08:08:16 -0600

    Completed Ch 4 & 5 Labs

---
+++ Lab/CH4/Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 24.5;
+            Console.WriteLine(Math.Round(num));
+        }
+    }
+}
---
+++ Lab/CH4/Practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+  enum Direction
+    {
+        North,
+        South,
+        East,
+        West
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 1;
+
+            string name = "Bob";
+            Direction userSelection = Direction.East;
+            switch(userSelection)
+            {
+                case Direction.East:
+                    Console.WriteLine("Move character East");
+                    break;
+                case Direction.North:
+                    Console.WriteLine("Hi Tim");
+                    break;
+                default:
+                    Console.WriteLine("I don't know how old you are");
+                    break;
+            }
+
+        }
+    }
+}
