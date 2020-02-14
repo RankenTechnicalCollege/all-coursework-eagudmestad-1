b4f2c25d9de6a1187a0563d2d14fe9b72b9f28aa
Evan Gudmestad <eagudmestad@ranken.edu>
2020-02-14 16:02:13 -0600

    Update

---
+++ Lab/CH12/Ch12Demo/Book2/ClassifiedAd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book2
+{
+    public class ClassifiedAd
+    {
+        //Instance Fields
+        private int _numWords;
+        private string _category;
+        private decimal _price;
+
+        //Property
+        public int NumWords
+        {
+            set
+            {
+                _numWords = value;
+                _price = _numWords * .09m;
+            }
+            get
+            {
+                return _numWords;
+            }
+        }
+
+        //Property
+        public string Category
+        {
+            set
+            {
+                _category = value;
+            }
+            get
+            {
+                return _category;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return _price;
+            }
+        }
+    }
+}
